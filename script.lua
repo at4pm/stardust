95b1d0642e621ced191c1963c2c77de056fec588
ata <lushstars.nathan@gmail.com>
2024-08-02 19:19:55 +1000

    Update script.lua

--- script.lua
+++ script.lua
@@ -12,7 +12,7 @@ Thank you for using Stardust. For more information, check the README on the gith
 discord.gg/GQbgKAMJad
 Developers: @at4pm, @.swabra.
 ]]--
-local checker = loadstring(game:HttpGet(""))()
+local checker = loadstring(game:HttpGet("https://gist.githubusercontent.com/at4pm/776ba1d3e74d20b110825c63055d8c2c/raw/97e49c613833e123f550bbc0f1f6d0b2b30cb186/stardust.lua"))()
 
 local key = ""
 
